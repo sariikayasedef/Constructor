8472379244ccfdfe0b1c931aba64eb745d03740b
Sedef SARIKAYA <sedefsarikaya.eem@gmail.com>
2021-06-20 20:31:18 +0300

    initial commit

---
+++ deneme.h
@@ -0,0 +1,8 @@
+#ifndef DENEME_HEADER_
+#define DENEME_HEADER_
+
+#include <stdint.h>
+
+#define MACRO   (uint8_t) 10U
+
+#endif
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "deneme.h"
+
+int main(int argc, char ** argv)
+{
+    printf("Hellow World => %d\n",MACRO);
+
+    return 0;
+}
